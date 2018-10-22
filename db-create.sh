c37b6f2d65d8e29bbdfd66480c433a68199f234b
Johan Pauwels <jpauwels@noreply.users.github.com>
2018-10-22 12:45:31 +0100

    Update MongoDB to v4.0

--- db-create.sh
+++ db-create.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker service create --network=func_functions --name faas-mongodb --env-file=env-file --mount type=volume,source=mongodb-dbdata,destination=/data/db --mount type=volume,source=mongodb-configdata,destination=/data/configdb --mount type=bind,source=${PWD}/db-setup.sh,destination=/docker-entrypoint-initdb.d/db-setup.sh mongo:3.6-jessie
+docker service create --network=func_functions --name faas-mongodb --env-file=env-file --mount type=volume,source=mongodb-dbdata,destination=/data/db --mount type=volume,source=mongodb-configdata,destination=/data/configdb --mount type=bind,source=${PWD}/db-setup.sh,destination=/docker-entrypoint-initdb.d/db-setup.sh mongo:4.0-xenial
