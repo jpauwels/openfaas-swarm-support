3c878e1e6e871bd9d73332132649e6a8502f0065
Johan Pauwels <jpauwels@users.noreply.github.com>
2020-09-24 17:55:29 +0100

    Name routes in Kong configuration for easier management

--- kong-configuration.sh
+++ kong-configuration.sh
@@ -4,10 +4,10 @@ function kong_admin_curl {
     docker exec $(docker ps -q -f name="kong\.") curl $@
 }
 kong_admin_curl -X POST http://localhost:8001/services/ --data 'name=openfaas' --data 'url=http://gateway:8080'
-kong_admin_curl -X POST http://localhost:8001/services/openfaas/routes --data 'paths[]=/function&paths[]=/async-function' --data 'strip_path=false'
+kong_admin_curl -X POST http://localhost:8001/services/openfaas/routes --data 'name=openfaas-functions' --data 'paths[]=/function&paths[]=/async-function' --data 'strip_path=false'
 
 kong_admin_curl -X POST http://localhost:8001/services/ --data 'name=qbc' --data 'url=http://qbc_api:80'
-kong_admin_curl -X POST http://localhost:8001/services/qbc/routes --data 'paths[]=/qbc' --data 'strip_path=false'
+kong_admin_curl -X POST http://localhost:8001/services/qbc/routes --data 'name=qbc' --data 'paths[]=/qbc' --data 'strip_path=false'
 
 kong_admin_curl -X POST http://localhost:8001/services/openfaas/plugins --data "name=cors" --data "config.origins=*" --data "config.methods[]=GET" --data "config.methods[]=POST" --data "config.headers=Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Auth-Token" --data "config.exposed_headers=X-Auth-Token" --data "config.credentials=true" --data "config.max_age=3600"
 
