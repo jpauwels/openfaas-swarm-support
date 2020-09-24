2e2f67541492138bcd4cbf7508c481509b81d164
Johan Pauwels <jpauwels@users.noreply.github.com>
2020-09-24 17:34:50 +0100

    Remove database initialisation script that creates users

--- db-setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-read -r -a USERS <<< ${MONGO_USERS}
-
-for ((i=0; i<"${#USERS[@]}"; i+=4));
-do
-    mongo --eval "db = db.getSiblingDB('${USERS[i]}'); db.createUser({user: '${USERS[i+1]}', pwd: '${USERS[i+2]}', roles: ['${USERS[i+3]}']});"
-done
