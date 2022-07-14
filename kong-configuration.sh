1bd04e82eaaebac2b634f34dc9590b6bc3d5693c
Johan Pauwels <jpauwels@users.noreply.github.com>
2022-07-14 10:06:41 +0100

    Update CORS configuration

--- kong-configuration.sh
+++ kong-configuration.sh
@@ -9,7 +9,7 @@ kong_admin_curl -X POST http://localhost:8001/services/openfaas/routes --data 'n
 kong_admin_curl -X POST http://localhost:8001/services/ --data 'name=qbc' --data 'url=http://qbc_api:80'
 kong_admin_curl -X POST http://localhost:8001/services/qbc/routes --data 'name=qbc' --data 'paths[]=/qbc' --data 'strip_path=false'
 
-kong_admin_curl -X POST http://localhost:8001/services/openfaas/plugins --data "name=cors" --data "config.origins=*" --data "config.methods[]=GET" --data "config.methods[]=POST" --data "config.headers=Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Auth-Token" --data "config.exposed_headers=X-Auth-Token" --data "config.credentials=true" --data "config.max_age=3600"
+kong_admin_curl -X POST http://localhost:8001/services/openfaas/plugins --data "name=cors" --data "config.origins=*" --data "config.methods=GET" --data "config.methods=POST" --data "config.headers=Accept" --data "config.headers=Accept-Version" --data "config.headers=Content-Length" --data "config.headers=Content-MD5" --data "config.headers=Content-Type" --data "config.headers=Date" --data "config.headers=X-Auth-Token" --data "config.exposed_headers=X-Auth-Token" --data "config.credentials=true" --data "config.max_age=3600"
 
 # Setup Letsencrypt HTTPS
 # kong_admin_curl -X POST http://localhost:8001/certificates -d "cert=$(cat /etc/letsencrypt/live/audio-analysis.eecs.qmul.ac.uk/fullchain.pem)" -d "key=$(cat /etc/letsencrypt/live/audio-analysis.eecs.qmul.ac.uk/privkey.pem)" -d "snis[]=audio-analysis.eecs.qmul.ac.uk"
