6de746c4c3e5f5e7890f2060ba5c29a93c4ff5d1
Johan Pauwels <jpauwels@users.noreply.github.com>
2022-07-14 11:48:13 +0100

    Add UniTrento config

--- minio-closeport.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker service update --publish-rm published=9000,target=9000 faas_minio
--- minio-openport.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker service update --publish-add published=9000,target=9000 faas_minio
