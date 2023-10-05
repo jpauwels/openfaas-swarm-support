8e986a41b90f4961b38846029717f17a0db2ce17
Johan Pauwels <jpauwels@users.noreply.github.com>
2023-10-05 15:10:02 +0100

    Update MongoDB and Minio images

--- db-exports.sh
+++ db-exports.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 MONGO_INITDB_ROOT_USERNAME=$(cat secrets/mongodb_root_username.txt)
 MONGO_INITDB_ROOT_PASSWORD=$(cat secrets/mongodb_root_password.txt)
-docker run --rm --network=func_functions mongo:5.0-focal sh -c "exec mongoexport --host faas_mongodb:27017 -u ${MONGO_INITDB_ROOT_USERNAME} -p ${MONGO_INITDB_ROOT_PASSWORD} --authenticationDatabase admin --db audiocommons --collection descriptors" > exports/audiocommons-descriptors-$(date -Iseconds).json
+docker run --rm --network=func_functions mongo:6-jammy sh -c "exec mongoexport --host faas_mongodb:27017 -u ${MONGO_INITDB_ROOT_USERNAME} -p ${MONGO_INITDB_ROOT_PASSWORD} --authenticationDatabase admin --db audiocommons --collection descriptors" > exports/audiocommons-descriptors-$(date -Iseconds).json
 #docker exec $(./db-instancename.sh) sh -c "exec mongoexport -u ${MONGO_INITDB_ROOT_USERNAME} -p ${MONGO_INITDB_ROOT_PASSWORD} --authenticationDatabase admin --db audiocommons --collection descriptors" > exports/audiocommons-descriptors-$(date -Iseconds).json
